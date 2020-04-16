37d51cd5847f27094f8f73c3c6397fd3b42086a7
Diane Reeves <dianehreeves@gmail.com>
2020-04-16 13:24:26 -0500

    most working except highscore

---
+++ questions.js
@@ -0,0 +1,42 @@
+
+
+//Questions and answers for quiz.//
+
+var questions = [
+    {
+        theQuestion: "Where can you insert JavaScript?",
+        choices: ["External files", "the <body>", "External url", "All of the above"],
+        answer: "All of the above"
+    },
+    {
+        theQuestion: "In web pages what is the global object?",
+        choices: ["window", "child", "function", "var"],
+        answer: "window"
+    },
+    {
+        theQuestion: "Which variable declaration is NOT used in Javascript?",
+        choices: ["var", "set", "let", "const"],
+        answer: "set"
+    },
+    {
+        theQuestion: "Which word below is a data type?",
+        choices: ["strings", "booleans", "numbers", "All of the above"],
+        answer: "All of the above"
+    },
+    {
+        theQuestion: "What company developed Javascript?",
+        choices: ["Sun Microsystems", "Netscape", "CERN", "IBM"],
+        answer: "Netscape"
+    },
+    {
+        theQuestion: "What kind of loop does Javascript NOT support?",
+        choices: ["for", "while", "for/in", "for/out"],
+        answer: "for/out"
+    },
+    {
+        theQuestion: "Who created Javascript?",
+        choices: ["John Resig", "Brendan Eich", "Hakon Wium Lie", "Larry Page"],
+        answer: "Brendan Eich"
+    }
+]
+
